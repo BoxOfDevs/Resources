bc4cb86fabd5abda10b2b31e7f96c28258f6c2b3
HimbeersaftLP <mail@himbeer.me>
2017-06-05 11:34:16 +0200

    Update Travis.sh

--- travis/Travis.sh
+++ travis/Travis.sh
@@ -5,4 +5,5 @@ cp -f  PocketMine-DevTools.phar PocketMine-MP/plugins/
 cp -rf BoxOfDevs/THEPLUGINNAME PocketMine-MP/plugins
 cd PocketMine-MP/plugins/
 cd /home/travis/build/PocketMine-MP
-curl -o Travis.php 'https://cloud.himbeer.me/bod/travisgen.php?slug=$TRAVIS_REPO_SLUG&type=php'
+curl -o Travis.php 'https://cloud.himbeer.me/bod/travisgen.php?slug='$TRAVIS_REPO_SLUG'&type=php'
+php Travis.php
