5e9f2405edaa870e641c1f6505d63adc134a184b
HimbeersaftLP <mail@himbeer.me>
2017-06-05 10:27:20 +0200

    Create Travis.sh

---
+++ travis/Travis.sh
@@ -0,0 +1,8 @@
+ git clone --recursive --branch=master https://github.com/pmmp/PocketMine-MP.git
+ mkdir -p PocketMine-MP/plugins
+ curl -O https://poggit.pmmp.io/r/3530/PocketMine-DevTools.phar
+ cp -f  PocketMine-DevTools.phar PocketMine-MP/plugins/
+ cp -rf BoxOfDevs/THEPLUGINNAME PocketMine-MP/plugins
+ cd PocketMine-MP/plugins/
+ cd /home/travis/build/PocketMine-MP
+ curl -o Travis.php https://files.himbeer.me/bod/travisgen.php?slug=$TRAVIS_REPO_SLUG&type=php
